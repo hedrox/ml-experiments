cdc93a0dd3e658d32091dffd14b990357f753275
Andrei Marin <hedrox53@gmail.com>
2016-10-09 21:18:33 +0300

    added basic save/restore model

--- nn/neuralnet.py
+++ nn/neuralnet.py
@@ -4,6 +4,21 @@ class NeuralNetwork(object):
     def __init__(self, layers):
         self.layers = layers
 
+    def add(self, layers):
+        self.layers += layers
+
+    def predict(self, x):
+        pass
+
+    def save(self, file_path):
+        layers = [{"W": self.layers.W, "b": self.layers.b} for layer in self.layers]
+        np.save(file_path, layers)
+
+    def restore(self, file_path):
+        layers = np.load(file_path)
+        for i in range(self.layers):
+            self.layers[i].W = layers[i]["W"]
+            self.layers[i].b = layers[i]["b"]
 
     def input_error(self, y):
         return y - self.layers[-1].output
