fixed backpropagation algorithm and added unittest for it

nn/layers.py
@@ -19,11 +19,17 @@ class Layer(object):
         self.a = self.activation(np.dot(layer_in, self.W.T) + self.b)
         return self.a
     
-    def bwd_prop(self, delta, last_layer_activation, previous_layer_w):
+    def bwd_prop(self, delta, layer_activation, layer_w):
+        out_delta = np.dot(delta, layer_w)
         grad = d_sigmoid(self.a)
-        delta = np.dot(previous_layer_w.T, delta) * grad
-        self.delta = np.dot(delta, last_layer_activation.T)
-        return self.delta
+        delta = out_delta * grad
+        dw = []
+        for delt in delta:
+            for activ in layer_activation:
+                dw.append(delt*activ)
+        dW = np.array(dw).reshape(layer_w.shape)
+        self.dW = dW
+        return delta
 
     @property
     def output(self):

nn/neuralnet.py
@@ -26,17 +26,24 @@ class NeuralNetwork(object):
                 # output layer error
                 out_error = self.output_error(y_output, y_pred)
                 delta = out_error * self.layers[-1].d_output(y_pred)
-                delta = np.dot(delta, self.layers[-1].a.T)
-                self.layers[-1].delta = delta
+                dw = []
+                for delt in delta:
+                    for activ in self.layers[-2].a:
+                        dw.append(delt*activ)
+                dW = np.array(dw).reshape(self.layers[-1].W.shape)
+                self.layers[-1].dW = dW
 
-                # other layers error
-                for layer in range(2,len(self.layers)):
-                    last_layer_activation = self.layers[-layer-1].a
-                    delta = self.layers[-layer].bwd_prop(delta, last_layer_activation, self.layers[-layer+1].W)
-                
+                # hidden layers error
+                for layer in reversed(range(1,len(self.layers)-1)):
+                    try:
+                        layer_activation = self.layers[layer-1].a
+                    except AttributeError:
+                        layer_activation = x_input
+                    delta = self.layers[layer].bwd_prop(delta, layer_activation, self.layers[layer+1].W)
+      
                 #SGD
                 for layer in self.layers[1:]:
-                    layer.W += -learning_rate * layer.delta
+                    layer.W -= learning_rate * layer.dW
             # for batch in range(batches):
             #     start_batch_idx = batch * batch_size
             #     end_batch_idx = start_batch_idx + batch_size

nn/test.py
@@ -16,8 +16,47 @@ class Network(unittest.TestCase):
         self.assertEqual('{0:.3f}'.format(y_pred[0]), '0.819')
         self.assertEqual('{0:.3f}'.format(y_pred[1]), '0.746')
 
+    def test_backpropagation(self):
+        X = np.array([0,1])
+        y_output = np.array([1,0])
+        y_pred = self.nn.predict(X)
 
-# layers = [layers.Layer(2,2), layers.Layer(2,2), layers.Layer(2,1)]
+        out_error = self.nn.output_error(y_output, y_pred)
+        self.assertEqual('{0:.3f}'.format(out_error[0]), '-0.181')
+        self.assertEqual('{0:.3f}'.format(out_error[1]), '0.746')
+
+        delta = out_error * self.nn.layers[-1].d_output(y_pred)
+        self.assertEqual('{0:.3f}'.format(delta[0]), '-0.027')
+        self.assertEqual('{0:.3f}'.format(delta[1]), '0.141')
+
+        dw = []
+        for delt in delta:
+            for activ in self.nn.layers[-2].a:
+                dw.append(delt*activ)
+        
+        output_dW = np.array(dw).reshape(self.nn.layers[-1].W.shape)
+        self.assertEqual('{0:.3f}'.format(output_dW[0][0]), '-0.021')
+        self.assertEqual('{0:.3f}'.format(output_dW[0][1]), '-0.011')
+        self.assertEqual('{0:.3f}'.format(output_dW[1][0]), '0.109')
+        self.assertEqual('{0:.3f}'.format(output_dW[1][1]), '0.060')
+        self.nn.layers[-1].dW = output_dW
+
+        # hidden layers error
+        for layer in reversed(range(1,len(self.nn.layers)-1)):
+            try:
+                layer_activation = self.nn.layers[layer-1].a
+            except AttributeError:
+                layer_activation = X
+            delta = self.nn.layers[layer].bwd_prop(delta, layer_activation, self.nn.layers[layer+1].W)
+        
+        hidden_dW = self.nn.layers[-2].dW
+        self.assertEqual('{0:.3f}'.format(hidden_dW[0][0]), '0.000')
+        self.assertEqual('{0:.3f}'.format(hidden_dW[0][1]), '0.003')
+        self.assertEqual('{0:.3f}'.format(hidden_dW[1][0]), '0.000')
+        self.assertEqual('{0:.3f}'.format(hidden_dW[1][1]), '0.033')
+    
+
+# layers = [layers.Layer(2,2,input_layer=True), layers.Layer(2,2), layers.Layer(2,2)]
 
 # X = np.array([[0,0],
 #               [0,1],
@@ -25,6 +64,10 @@ class Network(unittest.TestCase):
 #               [1,1]])
 
 # Y = np.array([0,1,1,0])
+
+# X = np.array([[0,1],[1,0]])
+# Y = np.array([[1,0],[1,1]])
+
 # nn = neuralnet.NeuralNetwork(layers)
 # nn.fit(X, Y)
 # for e in X: